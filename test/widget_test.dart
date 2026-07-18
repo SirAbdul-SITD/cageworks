a26ba4255f1f88233e6eecaa2e94aa1b02897e9b
abdulkarimhussain7 <abdulkarimhussain7@gmail.com>
2026-07-18 10:45:31 +0000

    Initial commit

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,7 +5,8 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-import 'package:chronoquiz/main.dart';
+
+import 'package:cageworks/main.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
@@ -13,7 +14,7 @@ import 'package:flutter_test/flutter_test.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const ChronoquizApp());
+    await tester.pumpWidget(const CageworksApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
